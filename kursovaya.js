fccc866d9eb451b5b1d37af70367cc5ddbfd1dad
ka3axctah777 <31667102+ka3axctah777@users.noreply.github.com>
2018-06-01 10:48:50 +0300

    Add files via upload

---
+++ kursovaya.js
@@ -0,0 +1,58 @@
+'use strict';
+const camelcase = require('camelcase');
+
+const keyWord = 'find';
+
+const assertion = {
+  WhereIs: (object, value) => object === value,
+  Empty: (object, value) => object.length === 0,
+};
+
+const assertNames = Object.keys(assertion);
+
+const wrap = arr => new Proxy(arr, {
+  get(target, propKey) {
+    if (propKey in target) return target[propKey];
+    const assertName = assertNames.find(assert =>
+      propKey.endsWith(assert));
+    if (propKey.startsWith(keyWord)) {
+      const field = camelcase(
+        propKey.substring(keyWord.length,
+          propKey.length - assertName.length)
+      );
+      const assert = assertion[assertName];
+      return value => target.find(item => assert(item[field], value));
+    }
+  }
+});
+
+const arr = wrap([
+  { car: 'Dacia', model: '1300', year: 1969 },
+  { car: 'Ferrari', model: '488GTB', year: 2016 },
+  { car: 'BMW', model: 'X5', year: 1999 },
+  {}
+]);
+
+console.log(arr.findCarWhereIs('BMW'));
+console.log(arr.findEmpty);
+
+const handler = {
+  set(obj, prop, value) {
+    if (typeof value !== 'string') {
+      throw new Error('only string is allowed');
+    } else {
+      obj[prop] = value;
+    }
+  }
+};
+
+const obj = {};
+const proxyObj = new Proxy(obj, handler);
+
+console.log(proxyObj);
+
+proxyObj.name = 'abcd';
+
+console.log(proxyObj);
+
+
